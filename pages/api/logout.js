a5d9bb8b788ca35cb0de59b219a98de9683137c0
Naeem Ahmad <naeem.ahmad.9m@gmail.com>
2021-04-03 13:52:16 +0600

    added salt in logout cookie

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -24,7 +24,7 @@ export default nextConnect({
   // );
   res.setHeader(
     "Set-Cookie",
-    "access_token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    `access_token=deleted${Math.random()}; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`
   );
   res.json({ user: null, success: true });
 });
